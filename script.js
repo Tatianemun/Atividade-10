cb4f3eb7d98e6377ec1e9264edaa7340472eacf7
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-03-11 11:48:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function calcula(){
+function test(){
     alert("JS maravilhoso vitaminado funcionando");
 }
 
 function calcula(){
+    //criar variável
     let num1;
-    num1=le ;
-    let
+    //recebe texto do html
+    num1=document.getElementById("numero1").value;
+    //transforma texto em numero real
+    let num1real=parseFloat(num1);
+    
+    let num2;
+    num2=document.getElementById("numero2").value;
+    //transformar texto para numero real
+    let num2real=parseFloat(num2);
+ 
+    let result;
+    result=num1real+num2real;//atualizar nomes variaveis
+
+    document.getElementById("resultado").innerHTML=result;
 }
